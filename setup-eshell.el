c08dd679061c4d827bec0251097facc1eb87cb7b
Dan Jacka <danjacka@gmail.com>
2013-12-09 14:53:06 +1300

    Note where 'poppy ./bin/instance fg' sometimes fails

--- setup-eshell.el
+++ setup-eshell.el
@@ -4,4 +4,10 @@
   (python-kill-python)
   (funcall 'run-python (apply 'eshell-flatten-and-stringify args) nil t))
 
+;; Note: 'poppy ./bin/instance fg' sometimes fails with "Searching for program:
+;; no such file or directory, ./bin/instance". The problem is in function
+;; comint-exec, where the with-current-buffer macro switches to the current
+;; *Python* buffer. That buffer's default-directory variable is used as the
+;; *current working directory.
+
 (provide 'setup-eshell)
