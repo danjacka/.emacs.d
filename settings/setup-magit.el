c6907a5361aa8c002742ec8ef3153eba37a771ef
Dan Jacka <danjacka@gmail.com>
2015-07-06 21:10:49 +1200

    Simpler magit config

--- settings/setup-magit.el
+++ settings/setup-magit.el
@@ -1,35 +1,14 @@
-;; Load git configurations
-;; For instance, to run magit-svn-mode in a project, do:
-;;
-;;     git config --add magit.extension svn
-;;
-(add-hook 'magit-mode-hook 'magit-load-config-extensions)
-
-;; C-x C-k to kill file on line
-
-(defun magit-kill-file-on-line ()
-  "Show file on current magit line and prompt for deletion."
-  (interactive)
-  (magit-visit-item)
-  (delete-current-buffer-file)
-  (magit-refresh))
-
-(define-key magit-status-mode-map (kbd "C-x C-k") 'magit-kill-file-on-line)
-
 ;; full screen magit-status
 
 (defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
   ad-do-it
   (delete-other-windows))
 
-(defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
+;; don't prompt me
 
-(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+(set-default 'magit-unstage-all-confirm nil)
+(set-default 'magit-stage-all-confirm nil)
+(set-default 'magit-revert-buffers 'silent)
 
 ;; full screen vc-annotate
 
@@ -47,26 +26,5 @@
        (delete-other-windows))
 
      (define-key vc-annotate-mode-map (kbd "q") 'vc-annotate-quit)))
-
-;; ignore whitespace
-
-(defun magit-toggle-whitespace ()
-  (interactive)
-  (if (member "-w" magit-diff-options)
-      (magit-dont-ignore-whitespace)
-    (magit-ignore-whitespace)))
-
-(defun magit-ignore-whitespace ()
-  (interactive)
-  (add-to-list 'magit-diff-options "-w")
-  (magit-refresh))
-
-(defun magit-dont-ignore-whitespace ()
-  (interactive)
-  (setq magit-diff-options (remove "-w" magit-diff-options))
-  (magit-refresh))
-
-(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
-
 
 (provide 'setup-magit)
