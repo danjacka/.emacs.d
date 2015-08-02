5a938dbde7fb80f45d0ac445d50f4110e10ea519
Dan Jacka <dan.jacka@xero.com>
2015-08-03 11:49:04 +1200

    Ignore node.js long module paths on Windows
    Stop the long paths from generating noisy warning messages in grep's
    output.

--- settings/windows.el
+++ settings/windows.el
@@ -6,4 +6,7 @@
 ;; utf-8-with-signature-dos to plain utf-8 after recognising encoding="utf-8".
 (setq auto-coding-functions nil)
 
+;; Ignore node.js module directories when searching (because paths are often too long)
+(setq grep-find-ignored-directories (add-to-list 'grep-find-ignored-directories "node_modules"))
+
 (provide 'windows)
