11ef61bd3d033b3b9b6cddca7a67d2aa1fe1e6f4
Dan Jacka <danjacka@gmail.com>
2025-01-06 17:06:32 +1300

    Less aggressive paredit key rebinding

--- packages/setup-paredit.el
+++ packages/setup-paredit.el
@@ -10,11 +10,7 @@
   ;; Rebind nasty paredit keys
   (--each '(("M-s"         "s-s"         paredit-splice-sexp)
             ("M-<up>"      "s-<up>"      paredit-splice-sexp-killing-backward)
-            ("M-<down>"    "s-<down>"    paredit-splice-sexp-killing-forward)
-            ("C-<right>"   "s-<right>"   paredit-forward-slurp-sexp)
-            ("C-<left>"    "s-<left>"    paredit-forward-barf-sexp)
-            ("C-M-<left>"  "s-S-<left>"  paredit-backward-slurp-sexp)
-            ("C-M-<right>" "s-S-<right>" paredit-backward-barf-sexp))
+            ("M-<down>"    "s-<down>"    paredit-splice-sexp-killing-forward))
     (-let [(original replacement command) it]
       (define-key paredit-mode-map (read-kbd-macro original) nil)
       (define-key paredit-mode-map (read-kbd-macro replacement) command))))
