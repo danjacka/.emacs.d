7306ab637c982f8176d9636dc4972dcd8379bcfd
Dan Jacka <danjacka@gmail.com>
2025-01-11 08:18:11 +1300

    Fix: quit fullscreen magit

--- packages/setup-magit.el
+++ packages/setup-magit.el
@@ -6,7 +6,9 @@
   (magit-no-confirm '(stage-all-changes
 		      unstage-all-changes))
 
-  :bind (("C-x m" . magit-status))
+  :bind (("C-x m" . magit-status)
+         (:map magit-status-mode-map
+               ("q" . magit-quit)))
 
   :config
   (wrap-fullscreen magit-status)
@@ -15,6 +17,21 @@
 (use-package git-timemachine
   :defer t
   :bind (("C-x v t" . git-timemachine)))
+
+(defun kill-magit-buffers ()
+  (let ((current (current-buffer)))
+    (dolist (buf (magit-mode-get-buffers))
+      (unless (eq buf current)
+        (kill-buffer buf)))))
+
+(defun magit-quit ()
+  "Like magit-mode-bury-buffer, but also restores the window
+configuration stored by magit-status-fullscreen"
+  (interactive)
+  (let ((prev my/previous-window-configuration))
+    (kill-magit-buffers)
+    (funcall magit-bury-buffer-function 'kill-buffer)
+    (when prev (register-val-jump-to prev nil))))
 
 (provide 'setup-magit)
 
