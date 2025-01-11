f1892a11918f3d813bfcdf42843aa51d0ab298f4
Dan Jacka <danjacka@gmail.com>
2025-01-11 17:11:19 +1300

    Balance windows

--- settings/navigation.el
+++ settings/navigation.el
@@ -6,8 +6,23 @@
 (global-set-key (kbd "S-<up>") 'windmove-up)
 (global-set-key (kbd "S-<down>") 'windmove-down)
 
+;: Keep them windows nice and balanced
+(global-set-key (kbd "C-x 0") 'my/delete-window)
+(global-set-key (kbd "C-x 3") 'my/split-window-right)
+
 ;; Implementations
 
 (use-package windmove)
+
+(defun my/delete-window ()
+  (interactive)
+  (delete-window)
+  (balance-windows))
+
+(defun my/split-window-right ()
+  (interactive)
+  (split-window-right)
+  (windmove-right)
+  (balance-windows))
 
 (provide 'navigation)
