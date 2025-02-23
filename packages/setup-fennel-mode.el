54f09602e2422dfaf04f157bcbd8efe72edbc7ed
Dan Jacka <danjacka@gmail.com>
2025-02-24 09:43:00 +1300

    Add fennel-mode

---
+++ packages/setup-fennel-mode.el
@@ -0,0 +1,6 @@
+;; Fennel is a programming language that brings together the simplicity, speed,
+;; and reach of Lua with the flexibility of a lisp syntax and macro system.
+
+(use-package fennel-mode)
+
+(provide 'setup-fennel-mode)
