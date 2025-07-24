fd61efc493930ca456ead998bfc7f777a8c1a17b
Dan Jacka <danjacka@gmail.com>
2025-07-25 08:56:28 +1200

    Add pikchr-mode

--- custom.el
+++ custom.el
@@ -4,11 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
+   '("01f347a923dd21661412d4c5a7c7655bf17fb311b57ddbdbd6fce87bd7e58de6" default))
  '(package-selected-packages
    '(anzu cider consult deadgrep diminish expand-region fennel-mode git-timemachine
           gptel ledger-mode magit markdown-mode multiple-cursors orderless
-          org-roam paredit smex spacemacs-theme vertico)))
+          org-roam paredit pikchr-mode smex spacemacs-theme vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ packages/setup-pikchr.el
@@ -0,0 +1,4 @@
+(use-package pikchr-mode
+  :defer t)
+
+(provide 'setup-pikchr-mode)
